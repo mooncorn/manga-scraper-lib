86b5c8040291f1a615e41aba07a9ed11d8ce413f
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2023-05-12 10:57:03 -0400

    Remove unused function

--- scrapers/src/index.ts
+++ scrapers/src/index.ts
@@ -1,12 +1,12 @@
-import puppeteer from "puppeteer-extra";
-import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import KissmangaScraper from "./sources/kissmanga";
-import MangakakalotScraper from "./sources/mangakakalot";
+import puppeteer from 'puppeteer-extra';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import KissmangaScraper from './sources/kissmanga';
+import MangakakalotScraper from './sources/mangakakalot';
 
 puppeteer.use(AdblockerPlugin()).use(StealthPlugin());
 
-puppeteer.launch({ headless: "new" }).then(async (browser) => {
+puppeteer.launch({ headless: 'new' }).then(async (browser) => {
   const sources = {
     kissmanga: new KissmangaScraper(browser),
     mangakakalot: new MangakakalotScraper(browser),
